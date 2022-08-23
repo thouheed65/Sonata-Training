b96ee2ab2461bf350c824da17fe403a031584cfc
Mohammed Thouheed <thouheed.md@sonata-software.com>
2022-08-23 14:02:23 +0530

    Added a logic of addition and its test case

---
+++ src/test/java/com/example/demo/AddInterface.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+public interface AddInterface {
+	public int add(int no1, int no2);
+
+}
---
+++ src/test/java/com/example/demo/AddTestCase.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class AddTestCase implements AddInterface {
+	@Override
+	public int add(int no1, int no2) {
+		// TODO Auto-generated method stub
+		return no1+no2;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+

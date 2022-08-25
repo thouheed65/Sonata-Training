a354abb50d3d39e6bd1b4a8fb2ff7327e98f4767
Mohammed Thouheed <thouheed.md@sonata-software.com>
2022-08-25 13:51:01 +0530

    orig

--- src/test/java/com/sonata/caltest/Caltest.java
+++ src/test/java/com/sonata/caltest/Caltest.java
@@ -1,9 +1,12 @@
 package com.sonata.caltest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import  org.junit.jupiter.api.Test;
+
 import com.sonata02.Calculator;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Test;
+
 
 public class Caltest {
 	Calculator c1 = new Calculator();
